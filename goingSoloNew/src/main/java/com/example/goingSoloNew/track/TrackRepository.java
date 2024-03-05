17b56dfc926edefa30deff1ed5ab210987fa51d4
chasekellogg12 <chasekellogg12@gmail.com>
2024-03-05 15:22:43 -0800

    umm

--- goingSoloNew/src/main/java/com/example/goingSoloNew/track/TrackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.goingSoloNew.track;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrackRepository extends JpaRepository<Track, String> {
-
-}
