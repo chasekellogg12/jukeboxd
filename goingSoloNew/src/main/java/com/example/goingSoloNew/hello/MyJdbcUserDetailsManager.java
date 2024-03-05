7783296d7f9f3851145f371ed2148715eff6b717
chasekellogg12 <chasekellogg12@gmail.com>
2024-03-05 15:21:10 -0800

    Saved?

--- goingSoloNew/src/main/java/com/example/goingSoloNew/hello/MyJdbcUserDetailsManager.java
+++
@@ -1,7 +0,0 @@
-package com.example.goingSoloNew.hello;
-
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-public class MyJdbcUserDetailsManager extends JdbcUserDetailsManager {
-
-}
