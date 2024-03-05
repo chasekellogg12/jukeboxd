73d56cdb1e177d7ed63016ccae1d95fb13285a99
chasekellogg12 <chasekellogg12@gmail.com>
2024-03-05 15:57:42 -0800

    j

--- goingSoloNew/src/main/java/com/example/goingSoloNew/hello/MyUserDetails.java
+++
@@ -1,8 +0,0 @@
-package com.example.goingSoloNew.hello;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface MyUserDetails extends UserDetails {
-	String getName();
-	String getEmail();
-}
--- goingSoloNew/src/main/java/com/example/goingSoloNew/myLike/MyLikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.goingSoloNew.myLike;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyLikeRepository extends JpaRepository<MyLike, Long> {
-
-}
