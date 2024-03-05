4cdd4cf3773033ccb730a5c1465dae9b61a44c38
chasekellogg12 <chasekellogg12@gmail.com>
2024-03-05 15:44:56 -0800

    wtf is happening

--- goingSoloNew/src/main/java/com/example/goingSoloNew/user/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.goingSoloNew.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<MyAuthority, String> {
-
-}
