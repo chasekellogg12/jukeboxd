86b8341ac68ac7dda6f5761a6d00a8216150ad66
chasekellogg12 <chasekellogg12@gmail.com>
2024-03-05 19:28:55 -0800

    added metadata to gitignore and allowed cors config with talend

--- goingSoloNew/src/main/java/com/example/goingSoloNew/CorsConfig.java
+++ goingSoloNew/src/main/java/com/example/goingSoloNew/CorsConfig.java
@@ -9,7 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override // this means that if WebMvcConfigurer has a method called "addCorsMapping()", use the method in this class isntead of that one
 	public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Adjust the mapping based on your backend API
-        	.allowedOrigins("http://localhost:3000")  // The origin of your React app
+//        	.allowedOrigins("http://localhost:3000")  // The origin of your React app
+        	.allowedOrigins("*")
         	.allowedMethods("GET", "POST", "PUT", "DELETE");
 	} // basically saying: from localhost:3000 can request GET, POST, PUT or DELETE from /
 }
